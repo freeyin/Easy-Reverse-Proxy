10b7ff7e7db5cd7527306eb0703d7a4658c875a3
freeyin <42679593+freeyin@users.noreply.github.com>
2025-08-07 22:11:51 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,10 @@ export let PORT = 377; // 端口
 
 export let PROXY_SECRET = 'Easy-Reverse-Proxy'; // 代理密钥
 // 代理
-export let PROXIES: Proxy[] = [];
+export let PROXIES: Proxy[] = [
+    {
+        domain: 'google.feijichang.me',
+        url: 'https://www.google.com.hk/',
+        enable: true
+    }
+];
