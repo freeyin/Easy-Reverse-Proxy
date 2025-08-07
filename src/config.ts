79d7a8237d0eacbbe3422d41afa4337bb46cdedf
freeyin <42679593+freeyin@users.noreply.github.com>
2025-08-07 22:15:01 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export let PROXY_SECRET = 'Easy-Reverse-Proxy'; // 代理密钥
 // 代理
 export let PROXIES: Proxy[] = [
     {
-        domain: 'google.feijichang.me',
+        domain: 'easy-reverse-proxy-omega.vercel.app',
         url: 'https://www.google.com.hk/',
         enable: true
     }
