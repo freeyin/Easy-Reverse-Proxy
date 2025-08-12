887fcf2747add910ab37d857a17c020a2b673bcb
freeyin <42679593+freeyin@users.noreply.github.com>
2025-08-12 14:15:09 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export let PROXY_SECRET = 'Easy-Reverse-Proxy'; // 代理密钥
 // 代理
 export let PROXIES: Proxy[] = [
     {
-        domain: 'youtube.feijichang.me',
-        url: 'https://www.youtube.com/',
+        domain: 'miss.diaomao250.dpdns.org',
+        url: 'https://missav.ws/',
         enable: true
     }
 ];
