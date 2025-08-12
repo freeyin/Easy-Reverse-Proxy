0077b588b2234b44880f036a6085d78147d64bfe
freeyin <42679593+freeyin@users.noreply.github.com>
2025-08-12 14:42:41 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export let PROXY_SECRET = 'Easy-Reverse-Proxy'; // 代理密钥
 // 代理
 export let PROXIES: Proxy[] = [
     {
-        domain: 'miss.diaomao250.dpdns.org',
+        domain: 'miss.feijichang.me',
         url: 'https://missav.ws/',
         enable: true
     }
